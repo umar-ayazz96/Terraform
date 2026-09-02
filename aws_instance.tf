9383c789870ca0d47bb9bad18874c8cc62146a01
umar-ayazz96 <umarayazkhan96@gmail.com>
2026-09-02 23:29:41 +0500

    updated instance

--- aws_instance.tf
+++ aws_instance.tf
@@ -1,14 +1,24 @@
-resource aws_instance ubuntu_ec2 {
-
+resource aws_instance webserver {
     ami = var.ec2_ami
     instance_type = var.ec2_instance_type
-    subnet_id = aws_subnet.prod_vpc_subnet1.id
     associate_public_ip_address = true
-    key_name = "Linux_KP"
-    vpc_security_group_ids = [aws_security_group.prod_vpc_sg.id]
+    availability_zone = "ap-south-1a"
+    key_name                             = "Linux_KP"
+    ebs_optimized = true
+    
+    credit_specification { 
+        cpu_credits = "unlimited" 
+    }
+    
+    root_block_device { 
+        delete_on_termination = true 
+        encrypted = false 
+        volume_size = 8 
+        volume_type = "gp3" 
+    }
 
     tags = {
-        Name = "linux_srvr"
+        Name = "webserver"
     }
 
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output instance_public_ip {
-    value = aws_instance.ubuntu_ec2.public_ip
+    value = aws_instance.webserver.public_ip
 }
 
 output instance_dns_name {
-    value = aws_instance.ubuntu_ec2.private_dns
+    value = aws_instance.webserver.private_dns
 }
